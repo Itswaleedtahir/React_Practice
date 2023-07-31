21ff4d57297f00ca059c09865f7a88d1f807e9b6
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-07-31 10:58:26 +0500

    Update cad.js

--- first_class/src/components/cad.js
+++ first_class/src/components/cad.js
@@ -1,15 +1,19 @@
 import './cad.css';
 
 function card(props){
-    const {title , description} = props;
-    return <div style={{height: "300px",
-    width : "250px",
-    border : "1px solid lightgray",
-    display: "inline-block",
-    padding: "10px",
-    margin: "10px"}}>
+    const {title , description,img} = props;
+    return <div
+    className="card" 
+    // style={{height: "300px",
+    // width : "250px",
+    // border : "1px solid lightgray",
+    // display: "inline-block",
+    // padding: "10px",
+    // margin: "10px"}}
+    >
         <h3> {title}</h3>
         <p>{description}</p>
+        <img src={img} alt="Card" id="image_card" style={{}}/>
     </div>
 }
 
