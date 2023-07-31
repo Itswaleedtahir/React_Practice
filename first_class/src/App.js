2cfed23201b6ff20775220dc4a0b6fe6435f6cbf
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-07-31 10:58:18 +0500

    Update App.js

--- first_class/src/App.js
+++ first_class/src/App.js
@@ -6,24 +6,28 @@ function App() {
 
   var cards= [
     {
-      id : 1,
+      id: 1,
       title: "First card",
-      description: "First card description"
+      description: "First card description",
+      img: "https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon-thumbnail.png" 
     },
     {
-      id : 2,
+      id: 2,
       title: "second card",
-      description: "second card description"
+      description: "second card description",
+      img: "https://w1.pngwing.com/pngs/885/534/png-transparent-green-grass-nodejs-javascript-react-mean-angularjs-logo-symbol-thumbnail.png" 
     },
     {
-      id : 3,
+      id: 3,
       title: "Third card",
-      description: "Third card description"
+      description: "Third card description",
+      img: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png"
     },
     {
-      id : 4,
+      id: 4,
       title: "Fourth card",
-      description: "Fourth card description"
+      description: "Fourth card description",
+      img: "https://1000logos.net/wp-content/uploads/2020/08/MongoDB-Logo.jpg"
     }
   ]
   return (
@@ -31,7 +35,7 @@ function App() {
       <h1 className='heading'>Hello its me waleed</h1>
       <p>{message}</p>
       <button style={{backgroundColor:"olive"}}>Click me</button><br/>
-      {cards.map((card) => <Card key = {card.id} title ={card.title} description={card.description} />)}
+      {cards.map((card) => <Card key = {card.id} title ={card.title} description={card.description} img = {card.img}/>)}
       </>
   );
 }
