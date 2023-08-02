da1aa76120fd2b2c375eee4b967655c764a85cd5
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-08-02 15:22:11 +0500

    Update cad.js

--- 3rd_class/src/components/cad.js
+++ 3rd_class/src/components/cad.js
@@ -1,13 +1,10 @@
 import './cad.css';
 
-function card(props){
-    const {title , description,img} = props;
+function card({children}){
     return <div
     className="card"
     >
-        <h3> {title}</h3>
-        <p>{description}</p>
-        <img src={img} alt="Card" id="image_card" />
+        {children}
     </div>
 }
 
