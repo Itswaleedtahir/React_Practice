e6ac21cebdd6775d2f8dc9c9f865e47893bdb673
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-08-10 12:27:19 +0500

    Delete cad.js

--- 4th_class/src/components/cad.js
+++
@@ -1,7 +0,0 @@
-function card({children , className}){
-    return <div className={className} style={{border:"1px solid lightgrey" , display:"inline-block", padding:"10px", borderRadius:"10px"}} >
-        {children}
-    </div>
-}
-
-export default card
