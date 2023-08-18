04ca5212dc3ae6200935d47f5ae684597cf635ae
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-08-18 17:13:48 +0500

    Create Header.js

---
+++ 5th_class/src/components/Header.js
@@ -0,0 +1,7 @@
+function header(){
+    return  <div style={{ backgroundColor:"lightskyblue", width:"100%", textAlign: "center", overflow:"auto"}}>
+            <h1>Posts APP</h1>
+            </div>
+}
+
+export default header
