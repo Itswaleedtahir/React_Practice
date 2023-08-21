5bb38dc76a447c127899c710eb3f146cb2d70180
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-08-21 16:58:19 +0500

    Create Navbar.js

---
+++ 6th_class/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import "../assets/css/Navbar.css"
+
+function Navbar(){
+    return <div className="navbar">
+            <h3>Brand Logo</h3>
+            <div>
+                <ul>
+                    <li ><a href="">Home</a></li>
+                    <li ><a href="">Users</a></li>
+                    <li ><a  href="">Shops</a></li>
+                </ul>
+            </div>
+    </div> 
+
+}
+
+export default Navbar
