17ec8f0bbcf673055ef835a0722ce2eec82c925d
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-08-21 16:58:43 +0500

    Create Users.js

---
+++ 6th_class/src/pages/Users.js
@@ -0,0 +1,10 @@
+import Navbar  from "../components/Navbar"
+
+function Dashboard(){
+    return <>
+    <Navbar/>
+    <h1 style={{textAlign:"center"}}>Welcome to the Dashboard!</h1>
+    </>
+}
+
+export default Dashboard
