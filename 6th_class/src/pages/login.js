057ce245db51761dd23cedcf19f8f1daab4d253b
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-08-29 16:31:28 +0500

    Create login.js

---
+++ 6th_class/src/pages/login.js
@@ -0,0 +1,13 @@
+import '../assets/css/login.css'
+import LoginForm from '../components/login'
+function Login(){
+    return <>
+        <div className="login-wrapper">
+
+            <LoginForm/>
+
+        </div>
+    </>
+}
+
+export default Login
