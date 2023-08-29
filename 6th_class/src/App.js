e696884701717e507c2af4122b0a3ee0678bb003
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-08-29 16:31:03 +0500

    Update App.js

--- 6th_class/src/App.js
+++ 6th_class/src/App.js
@@ -1,15 +1,21 @@
 import Dashboard from './pages/Dashboard';
 import Users from './pages/Users';
-import { Routes , Route } from "react-router"
+import UserDetails from './pages/UserDetails';
+import { Routes , Route, Navigate } from "react-router"
 import { BrowserRouter } from "react-router-dom"
+import NotFound from './pages/404';
+import Login from './pages/login';
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Dashboard/> } />
+          <Route path='/' element={<Navigate to={'/login'}/>} />
           <Route path='/dashboard' element={<Dashboard/>} />
           <Route path='/users' element={ <Users/>} />
-        </Routes>
+          <Route path='/users/:userId' element={ <UserDetails/>} />
+          <Route path='/login' element={ <Login/> } />
+          <Route path='*'  element={<NotFound/>} />
+        </Routes>  
     </BrowserRouter>
   );
 }
